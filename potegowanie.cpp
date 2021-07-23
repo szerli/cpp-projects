0b279fcc2ae4bf0b609bc5c51bbe757055e7ff83
Kacper Mizera <76234579+szerli@users.noreply.github.com>
2021-07-23 02:17:32 +0200

    Add files via upload

---
+++ potegowanie.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int podstawa, wykladnik, wynik=1;
+	cout << "Podaj podstawe ktora chcesz spotegowac: " << endl;
+	cin >> podstawa;
+	cout << "Podaj wykladnik potegi: " << endl;
+	cin >> wykladnik;
+	for(int i=0; i<wykladnik; i++)
+	{
+		wynik=wynik*podstawa;
+	}
+	cout << "Wynikiem potegowania jest:\n" << wynik;
+	cout << "\n";
+
+	return 0;
+}
---
+++ witanie.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+
+{
+   
+    string imie;
+    string nazwisko;
+
+    cout << "Podaj swoje imie: " << endl; 
+    cin >> imie;
+    cout << endl;
+    
+    cout << "Podaj swoje nazwisko: " << endl;
+    cin >> nazwisko;
+    cout << endl;
+    
+    cout << "Milo mi Cie poznac" << ' '; 
+    cout << imie << endl << endl; 
+
+}
